c507f2deaecaf3b8f6c1c754be52fe8717f17efa
Hyun <hyun@balancehero.com>
2021-05-04 14:37:25 +0900

    procedure

--- delete.sql
+++ delete.sql
@@ -4,4 +4,12 @@ DELETE f
 FROM foo f
          INNER JOIN dictionary d
                     ON d.id=f.id
-WHERE d.stat = 1
+WHERE d.stat = 1;
+
+# Delete with join (if database is not selected)
+# !!it's not working with alias.!!
+DELETE home.foo
+FROM home.foo
+         INNER JOIN home.dictionary
+                    ON home.dictionary.id=home.foo.id
+WHERE home.dictionary.stat = 1;
